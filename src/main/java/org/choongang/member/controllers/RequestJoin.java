83ec692f03885008130a910c96508d328078133f
Mi-Hee <eeh716@naver.com>
2024-02-23 09:56:03 +0900

    회원가입 구현중

--- src/main/java/org/choongang/member/controllers/RequestJoin.java
+++ src/main/java/org/choongang/member/controllers/RequestJoin.java
@@ -3,6 +3,7 @@ package org.choongang.member.controllers;
 import jakarta.validation.constraints.AssertTrue;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
@@ -12,6 +13,7 @@ public class RequestJoin {
     private String email;
 
     @NotBlank
+    @Size(min = 8)
     private String password;
 
     @NotBlank
